e4ea3af272e316502c8221adfeb9682791c4f58f
Garred <gmurphy@csumb.edu>
2017-09-30 23:16:00 -0700

    UI Manager changes

--- BGE/UIManager.cs
+++ BGE/UIManager.cs
@@ -3,25 +3,25 @@ using System.Collections.Generic;
 using System;
 
 
+/* Garred Murphy */
 
 namespace ASCIIGameEngine
 {
 	public class UIManager {
-		string playersHealth = "";
-		int playersScore = 0;
-
-		public	UIManager()	{	
+		string playersHealth = "";  //health probably should not be stored as a string.  
+        int playersScore = 0;  //And score probably should not be stored as an int because elsewhere in the code these are both writen as doubles. 
+                               //  I didn't want to change the code the others had written so i left it as it was.
+        public UIManager()	{	
 			MessageBus.getInstance ().OnPlayerHealthEvent += HandlePlayerHealth;
 			MessageBus.getInstance().OnPlayerScoreEvent += HandlePlayerScore;
-		}	
+		}
 		void HandlePlayerHealth(PlayerHealthEvent e){
 			playersHealth = e.health.ToString ();
 		}
 		void HandlePlayerScore(PlayerScoreEvent e){
-			playersHealth = e.score.ToString ();
+			playersScore = (int)e.score;  //
 		}
 		private	void Update()	{
-            //UIToRender(playersHealth, playersScore);
 
             MessageBus.getInstance().AddEvent(new UIToRenderEvent(playersHealth, playersScore));
 
